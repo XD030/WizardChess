2c18f760652639d5e1b57967a69ff0a7fd1e3020
flosxom <hanslai8711@gmail.com>
2025-11-28 11:29:41 +0800

    use legacy ReactDOM.render instead of react-dom/client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from "react-dom/client";
+// client/src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
