de081daacc861acf414df4d73df48ce3a0aecdb9
flosxom <44235512+XD030@users.noreply.github.com>
2025-12-14 23:29:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
-// vite.config.ts
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-});
 
 
 // ⭐ 這三行取代 __dirname，完全符合 ESM 規範
@@ -18,12 +10,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  
   plugins: [react()],
 
   root: "client",
 
   build: {
     outDir: "../dist/client",
+    sourcemap: true,
   },
 
   resolve: {
